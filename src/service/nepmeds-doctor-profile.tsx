35038c89bcbf65bebfe3f0b3d8d649485977bcbd
Roshan Gyawali <roshan.gyawali@codehimalaya.tech>
2023-05-24 10:09:07 +0545

    fix: interface of doctor-profile api

--- src/service/nepmeds-doctor-profile.tsx
+++ src/service/nepmeds-doctor-profile.tsx
@@ -2,15 +2,38 @@ import { useQuery } from "react-query";
 import { NepMedsResponse, api } from "./service-api";
 import { HttpClient } from "./service-axios";
 
+interface IUser {
+  first_name: string;
+  middle_name: string;
+  last_name: string;
+  email: string;
+  mobile_number: string;
+  address: string;
+}
+interface IDoctorAcademicInfo {
+  degree_program: string;
+  graduation_year: number;
+  university: string;
+  major: string;
+  file: string;
+}
+interface IDoctorCertificationInfo {
+  title: string;
+  issued_by: string;
+  certification_name: string;
+  file: string;
+  certification_issued_data: string;
+}
+interface IDoctorExperience {
+  hospital: string;
+  description: string;
+  currently_working: boolean;
+  from_date: string;
+  to_date: string;
+  file: string;
+}
 interface IGetDoctorProfile {
-  user: {
-    first_name: string;
-    middle_name: string;
-    last_name: string;
-    email: string;
-    mobile_number: string;
-    address: string;
-  };
+  user: IUser;
   image: string;
   province: number;
   district: number;
@@ -22,15 +45,9 @@ interface IGetDoctorProfile {
   citizenship_number: string;
   citizenship_issued_district: number;
   citizenship_issued_date: string;
-  doctor_academic_info: [
-    {
-      degree_program: string;
-      graduation_year: number;
-      university: string;
-      major: string;
-      file: string;
-    }
-  ];
+  doctor_academic_info: IDoctorAcademicInfo[];
+  doctor_certification_info: IDoctorCertificationInfo[];
+  doctor_experience: IDoctorExperience[];
 }
 
 const getDoctorProfile = async () => {
