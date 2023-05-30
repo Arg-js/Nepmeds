8c010e246f72a91fe6775328cef68a2545c15228
Smna <ghimiresamana666@gmail.com>
2023-05-30 11:04:32 +0545

    refactor(edit doctor profile): refactored edit doctor profile

---
+++ src/components/DocProfile/EditDoctor/EditBasic.tsx
@@ -0,0 +1,157 @@
+import { EditIcon } from "@chakra-ui/icons";
+import {
+  Card,
+  CardBody,
+  Flex,
+  Icon,
+  HStack,
+  Button,
+  VStack,
+  useDisclosure,
+  Text,
+  Box,
+  Image,
+} from "@chakra-ui/react";
+import { svgs } from "@nepMeds/assets/svgs";
+import ModalComponent from "@nepMeds/components/Form/ModalComponent";
+import { BasicInfoForm } from "@nepMeds/components/FormComponents";
+import { toastFail, toastSuccess } from "@nepMeds/components/Toast";
+import { IGetDoctorProfile } from "@nepMeds/service/nepmeds-doctor-profile";
+import { useUpdatePersonalInfoRegister } from "@nepMeds/service/nepmeds-register";
+import { colors } from "@nepMeds/theme/colors";
+import { useForm, FormProvider } from "react-hook-form";
+
+const EditBasic = ({
+  doctorProfileData,
+}: {
+  doctorProfileData: IGetDoctorProfile;
+}) => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const formMethods = useForm();
+  const updatePersonalInfo = useUpdatePersonalInfoRegister();
+  const onSavePersonalInfo = async () => {
+    try {
+      const isValid = formMethods.trigger();
+      if (!isValid) return;
+
+      await updatePersonalInfo.mutateAsync({
+        image: formMethods.getValues("image"),
+        bio_detail: formMethods.getValues("bio_detail"),
+        title: formMethods.getValues("title"),
+        user: {
+          first_name: formMethods.getValues("first_name"),
+          middle_name: formMethods.getValues("middle_name"),
+          last_name: formMethods.getValues("last_name"),
+        },
+      });
+      onClose();
+      toastSuccess("Personal information updated successfully!");
+    } catch (error) {
+      toastFail("Failed to update personal information!");
+    }
+  };
+  return (
+    <>
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        variant="outline"
+        mb={"18px"}
+        p={4}
+      >
+        <Image
+          w={"159px"}
+          h={"159px"}
+          src={doctorProfileData?.image}
+          fallbackSrc="https://via.placeholder.com/159"
+          alt="Caffe Latte"
+        />
+
+        <CardBody w={"100%"}>
+          <Box display={"flex"} justifyContent={"space-between"}>
+            <Text
+              fontWeight={"700"}
+              fontSize={"26.8085px"}
+              lineHeight={"32px"}
+              color={colors?.dark_1}
+              mb={"4px"}
+            >
+              {doctorProfileData?.user?.first_name}&nbsp;
+              {doctorProfileData?.user?.middle_name}&nbsp;
+              {doctorProfileData?.user?.last_name}&nbsp; (
+              {doctorProfileData?.specialization?.[0]})
+            </Text>
+            <Flex
+              alignItems={"center"}
+              justifyContent={"center"}
+              onClick={onOpen}
+              cursor="pointer"
+            >
+              <Icon as={EditIcon} boxSize={5} color={colors?.main} mr={"8px"} />
+              <Text
+                color={colors?.main}
+                fontWeight={"400"}
+                fontSize={"16px"}
+                lineHeight={"19px"}
+              >
+                Edit
+              </Text>
+            </Flex>
+          </Box>
+
+          <Text
+            fontWeight={"400"}
+            fontSize={"16px"}
+            lineHeight={"28px"}
+            color={"#5B5B5B"}
+          >
+            {/* Dentists are highly trained individuals who work with patients to
+              ensure that their teeth, gums, and mouth are healthy. Dentists are
+              highly trained individuals who work with patients to ensure that
+              their teeth, gums, and mouth are healthy.Dentists are highly
+              trained individuals who work with patients to ensure that their
+              teeth, gums, and mouth are healthy. */}
+            {doctorProfileData?.bio_detail}
+          </Text>
+          <ModalComponent
+            size="xl"
+            isOpen={isOpen}
+            onClose={onClose}
+            heading={
+              <HStack>
+                <svgs.logo_small />
+                <Text>Edit Personal Information</Text>
+              </HStack>
+            }
+            footer={
+              <HStack w="100%" gap={3}>
+                <Button variant="outline" onClick={onClose} flex={1}>
+                  Discard
+                </Button>
+                <Button
+                  flex={1}
+                  onClick={onSavePersonalInfo}
+                  background={colors.primary}
+                  color={colors.white}
+                >
+                  Save
+                </Button>
+              </HStack>
+            }
+          >
+            <VStack>
+              <FormProvider {...formMethods}>
+                <BasicInfoForm
+                  hidePasswordField={false}
+                  doctorProfileData={doctorProfileData}
+                  isEditable={true}
+                />
+              </FormProvider>
+            </VStack>
+          </ModalComponent>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default EditBasic;
---
+++ src/components/DocProfile/EditDoctor/EditCertification.tsx
@@ -0,0 +1,309 @@
+import { EditIcon } from "@chakra-ui/icons";
+import {
+  Card,
+  CardBody,
+  HStack,
+  Button,
+  VStack,
+  useDisclosure,
+  Text,
+  Box,
+  Center,
+  Grid,
+  GridItem,
+  Icon,
+} from "@chakra-ui/react";
+import { svgs } from "@nepMeds/assets/svgs";
+import ModalComponent from "@nepMeds/components/Form/ModalComponent";
+import { CertificationInfoForm } from "@nepMeds/components/FormComponents";
+import { toastFail, toastSuccess } from "@nepMeds/components/Toast";
+import { useUpdateCertificateInfo } from "@nepMeds/service/nepmeds-certificate";
+import {
+  IDoctorCertificationInfo,
+  IGetDoctorProfile,
+} from "@nepMeds/service/nepmeds-doctor-profile";
+import { normalURL } from "@nepMeds/service/service-axios";
+import { colors } from "@nepMeds/theme/colors";
+import { useForm, FormProvider } from "react-hook-form";
+import { Download } from "react-iconly";
+
+const EditCertification = ({
+  doctorProfileData,
+}: {
+  doctorProfileData: IGetDoctorProfile;
+}) => {
+  const {
+    isOpen: isCertificateOpen,
+    onClose: onCertificateClose,
+    onOpen: onCertificateOpen,
+  } = useDisclosure();
+  const formMethods = useForm();
+  const updateCertificateInfo = useUpdateCertificateInfo();
+
+  const onSaveCertificateInfo = async () => {
+    try {
+      const isValid = formMethods.trigger();
+      if (!isValid) return;
+      const allCertifications = formMethods.getValues("certification") || [];
+
+      for (let i = 0; i < allCertifications.length; i++) {
+        await updateCertificateInfo.mutateAsync({
+          data: {
+            doctor: formMethods.getValues("doctor"),
+            certificate_issued_date: formMethods.getValues(
+              `certification.${i}.certificate_issued_date`
+            ),
+            certificate_number: formMethods.getValues(
+              `certification.${i}.certificate_number`
+            ),
+            title: formMethods.getValues(`certification.${i}.title`),
+            issued_by: formMethods.getValues(`certification.${i}.issued_by`),
+            file: formMethods.getValues(`certification.${i}.file`),
+          },
+          id: doctorProfileData?.doctor_certification_info?.[i]?.id,
+        });
+      }
+      onCertificateClose();
+      toastSuccess("Certification information updated successfully!");
+    } catch (error) {
+      toastFail("Failed to update Certification information!");
+    }
+  };
+  return (
+    <>
+      <Card mb={"18px"}>
+        <Box p={"20px"} display={"flex"} justifyContent={"space-between"}>
+          <Text
+            fontWeight={"700"}
+            fontSize={"18px"}
+            lineHeight={"22px"}
+            color={colors?.primary_dark1}
+          >
+            Certification Info
+          </Text>
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            onClick={onCertificateOpen}
+            cursor="pointer"
+          >
+            <Icon as={EditIcon} boxSize={5} color={colors?.main} mr={"8px"} />
+            <Text
+              color={colors?.main}
+              fontWeight={"400"}
+              fontSize={"16px"}
+              lineHeight={"19px"}
+            >
+              Edit
+            </Text>
+          </Box>
+        </Box>
+        <CardBody>
+          <ModalComponent
+            size="xl"
+            isOpen={isCertificateOpen}
+            onClose={onCertificateClose}
+            heading={
+              <HStack>
+                <svgs.logo_small />
+                <Text>Edit Certificate Information</Text>
+              </HStack>
+            }
+            footer={
+              <HStack w="100%" gap={3}>
+                <Button variant="outline" onClick={onCertificateClose} flex={1}>
+                  Discard
+                </Button>
+                <Button
+                  flex={1}
+                  onClick={onSaveCertificateInfo}
+                  background={colors.primary}
+                  color={colors.white}
+                >
+                  Save
+                </Button>
+              </HStack>
+            }
+          >
+            <VStack>
+              <FormProvider {...formMethods}>
+                <CertificationInfoForm
+                  doctorProfileData={doctorProfileData}
+                  isEditable={true}
+                />
+              </FormProvider>
+            </VStack>
+          </ModalComponent>
+          <Grid
+            templateColumns={
+              doctorProfileData?.doctor_certification_info?.length
+                ? "repeat(3, 1fr)"
+                : ""
+            }
+          >
+            {doctorProfileData?.doctor_certification_info?.length ? (
+              doctorProfileData?.doctor_certification_info?.map(
+                (singleCertificationInfo: IDoctorCertificationInfo) => {
+                  const fileURL = singleCertificationInfo?.file
+                    ? `${normalURL}${singleCertificationInfo?.file}`
+                    : "";
+                  return (
+                    <>
+                      <GridItem w="100%">
+                        <VStack spacing={4} align="stretch">
+                          <Box display={"flex"} alignItems={"center"} gap={3}>
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"14px"}
+                              lineHeight={"16px"}
+                              letterSpacing={"0.4px"}
+                              color={"#4D4D4D"}
+                              w={"90px"}
+                            >
+                              Title
+                            </Text>
+
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"16px"}
+                              lineHeight={"19px"}
+                              color={colors?.black}
+                            >
+                              :&nbsp;{singleCertificationInfo?.title}
+                            </Text>
+                          </Box>
+                          <Box display={"flex"} alignItems={"center"} gap={3}>
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"14px"}
+                              lineHeight={"16px"}
+                              letterSpacing={"0.4px"}
+                              color={"#4D4D4D"}
+                              w={"90px"}
+                            >
+                              Issued Date
+                            </Text>
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"16px"}
+                              lineHeight={"19px"}
+                              color={colors?.black}
+                            >
+                              :&nbsp;
+                              {singleCertificationInfo?.certificate_issued_date}
+                            </Text>
+                          </Box>
+                        </VStack>
+                      </GridItem>
+                      <GridItem w="100%">
+                        <VStack spacing={4} align="stretch">
+                          <Box display={"flex"} alignItems={"center"} gap={3}>
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"14px"}
+                              lineHeight={"16px"}
+                              letterSpacing={"0.4px"}
+                              color={"#4D4D4D"}
+                              w={"86px"}
+                            >
+                              Issued By
+                            </Text>
+
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"16px"}
+                              lineHeight={"19px"}
+                              color={colors?.black}
+                            >
+                              :&nbsp;{singleCertificationInfo?.issued_by}
+                            </Text>
+                          </Box>
+                          <Box display={"flex"} gap={3}>
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"14px"}
+                              lineHeight={"16px"}
+                              letterSpacing={"0.4px"}
+                              color={"#4D4D4D"}
+                              w={"86px"}
+                            >
+                              Document
+                            </Text>
+                            <Box display={"flex"}>
+                              <Text
+                                fontWeight={"400"}
+                                fontSize={"14px"}
+                                lineHeight={"23px"}
+                                color={colors?.gray_700}
+                                mr={"8px"}
+                              >
+                                <a
+                                  href={fileURL}
+                                  target="_blank"
+                                  rel="noreferrer"
+                                  style={{
+                                    color: colors.primary,
+                                    display: "flex",
+                                    gap: 4,
+                                    alignItems: "center",
+                                  }}
+                                  download
+                                >
+                                  {singleCertificationInfo?.file
+                                    ?.split("/")
+                                    .pop()}
+                                  <Download
+                                    set="light"
+                                    primaryColor={colors?.main}
+                                    size={20}
+                                  />
+                                </a>
+                              </Text>
+                            </Box>
+                          </Box>
+                        </VStack>
+                      </GridItem>
+                      <GridItem w="100%">
+                        <VStack spacing={4} align="stretch">
+                          <Box display={"flex"} alignItems={"center"} gap={3}>
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"14px"}
+                              lineHeight={"16px"}
+                              letterSpacing={"0.4px"}
+                              color={"#4D4D4D"}
+                              w={"137px"}
+                            >
+                              Credential ID
+                            </Text>
+
+                            <Text
+                              fontWeight={"500"}
+                              fontSize={"16px"}
+                              lineHeight={"19px"}
+                              color={colors?.black}
+                            >
+                              :&nbsp;
+                              {singleCertificationInfo?.certificate_number}
+                            </Text>
+                          </Box>
+                        </VStack>
+                      </GridItem>
+                    </>
+                  );
+                }
+              )
+            ) : (
+              <Center>
+                <Text>No Data Found</Text>
+              </Center>
+            )}
+          </Grid>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default EditCertification;
