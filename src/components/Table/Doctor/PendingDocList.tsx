eea281bfc6fd439b1f306e23f7a3f6205d6ea59b
notThePrabesh <prabeshpathak50@gmail.com>
2023-07-25 16:22:03 +0545

    fix(dashboard): dashboard admin pending list

--- src/components/Table/Doctor/PendingDocList.tsx
+++ src/components/Table/Doctor/PendingDocList.tsx
@@ -5,12 +5,10 @@ import {
   Button,
   Center,
   HStack,
-  Icon,
   Input,
   InputGroup,
   InputLeftElement,
   Spinner,
-  Tag,
   Text,
   VStack,
   useDisclosure,
@@ -18,40 +16,32 @@ import {
 import { yupResolver } from "@hookform/resolvers/yup";
 import { svgs } from "@nepMeds/assets/svgs";
 import { DataTable } from "@nepMeds/components/DataTable";
+import { pendingColumns } from "@nepMeds/components/DataTable/columns";
 import FloatingLabelInput from "@nepMeds/components/Form/FloatingLabelInput";
 import ModalComponent from "@nepMeds/components/Form/ModalComponent";
 import Select from "@nepMeds/components/Form/Select";
 import { STATUSTYPE } from "@nepMeds/config/enum";
 import { useDebounce } from "@nepMeds/hooks/useDebounce";
-import { NAVIGATION_ROUTES } from "@nepMeds/routes/routes.constant";
 import { useDoctorList } from "@nepMeds/service/nepmeds-doctorlist";
-import { Specialization } from "@nepMeds/service/nepmeds-specialization";
 import { colors } from "@nepMeds/theme/colors";
-import { CellContext, PaginationState } from "@tanstack/react-table";
-import React, { useState } from "react";
+import { PaginationState } from "@tanstack/react-table";
+import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
-import { Show } from "react-iconly";
 import { IoFunnelOutline } from "react-icons/io5";
-import { generatePath, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import { ISpecializationList } from "./DoctorsList";
 
-interface CellContextSearch {
-  user: {
-    first_name: string;
-    middle_name: string;
-    last_name: string;
-  };
-}
 interface Props {
-  specializationList: ISpecializationList[];
+  specializationList?: ISpecializationList[];
+  showFilter?: boolean;
 }
 
 const schema = yup.object().shape({
   remarks: yup.string().required("Remarks  is required!"),
 });
 
-const PendingDocList = ({ specializationList }: Props) => {
+const PendingDocList = ({ specializationList, showFilter = true }: Props) => {
   const [{ pageIndex, pageSize }, setPagination] = useState<PaginationState>({
     pageIndex: 0,
     pageSize: 10,
@@ -91,122 +81,6 @@ const PendingDocList = ({ specializationList }: Props) => {
   const formMethods = useForm({ resolver: yupResolver(schema) });
 
   const navigate = useNavigate();
-  const columns = React.useMemo(
-    () => [
-      {
-        header: "S.N",
-        accessorFn: (_cell: CellContext<any, any>, index: number) => {
-          return index + 1;
-        },
-      },
-      {
-        header: "Doctor's Name",
-        accessorKey: "first_name",
-        accessorFn: (_cell: CellContextSearch) => {
-          return _cell?.user?.first_name + " " + _cell?.user?.last_name;
-        },
-      },
-      {
-        header: "Contact Number",
-        cell: ({
-          row,
-        }: CellContext<
-          {
-            user: IBasicInfo;
-          },
-          any
-        >) => {
-          const { mobile_number } = row?.original?.user ?? "";
-
-          return <p>{mobile_number}</p>;
-        },
-      },
-      {
-        header: "Specialization",
-        accessorKey: "specialization",
-        cell: ({
-          row,
-        }: CellContext<{ specialization_names: Specialization[] }, any>) => {
-          const specialization = row?.original?.specialization_names?.map(
-            data => (
-              <Tag key={data.id} color={colors.main} bg={"#c4d2e8"} mx={"1px"}>
-                {data.name}
-              </Tag>
-            )
-          );
-          return (
-            <Box
-              display={"flex"}
-              flexWrap={"wrap"}
-              width={"fit-content"}
-              p={1}
-              // background={colors.grey}
-              // borderRadius={20}
-            >
-              <p>{specialization}</p>
-            </Box>
-          );
-        },
-      },
-      // {
-      //   header: "Status",
-      //   accessorKey: "profile_status",
-      //   cell: ({ row }: CellContext<{ is_approved: boolean }, any>) => {
-      //     const { is_approved } = row.original;
-      //     return (
-      //       <Badge
-      //         colorScheme={is_approved ? "green" : "red"}
-      //         p={1}
-      //         borderRadius={20}
-      //         fontSize={11}
-      //         w={24}
-      //         textAlign="center"
-      //         textTransform="capitalize"
-      //       >
-      //         {is_approved ? "Approved" : "Not approved"}
-      //       </Badge>
-      //     );
-      //   },
-      // },
-      {
-        header: "Actions",
-        accessorKey: "actions",
-        cell: (cell: CellContext<any, any>) => {
-          return (
-            <HStack>
-              <Icon
-                as={Show}
-                fontSize={20}
-                cursor="pointer"
-                onClick={() => {
-                  formMethods.reset(cell.row.original);
-                  // onDetailsModalOpen();
-                  navigate(
-                    generatePath(NAVIGATION_ROUTES.DOC_PROFILE, {
-                      id: cell.row.original.id,
-                    })
-                  );
-                }}
-              />
-              {/* <Icon
-                as={Delete}
-                fontSize={20}
-                cursor="pointer"
-                color={colors.red}
-                onClick={() => {
-                  handleDeleteDoctor(cell.row.original.id);
-                  // formMethods.reset(cell.row.original);
-                  // onDetailsModalOpen();
-                  // setId(cell.row.original.id);
-                }}
-              /> */}
-            </HStack>
-          );
-        },
-      },
-    ],
-    []
-  );
 
   const {
     isOpen: isModalOpen,
@@ -270,7 +144,7 @@ const PendingDocList = ({ specializationList }: Props) => {
                 name="Specialization"
                 required
                 register={formMethods.register}
-                options={specializationList}
+                options={specializationList ?? []}
               />
               <Box display={"flex"} width={"100%"}>
                 <FloatingLabelInput
@@ -296,34 +170,36 @@ const PendingDocList = ({ specializationList }: Props) => {
       <HStack justifyContent="space-between">
         <Text fontWeight="medium">Pending Doctors</Text>
 
-        <HStack>
-          <InputGroup w="190px" borderColor={colors.grey_dark}>
-            <InputLeftElement pointerEvents="none" h={8}>
-              <SearchIcon color={colors.grey_dark} boxSize={4} />
-            </InputLeftElement>
-            <Input
-              w={40}
+        {showFilter && (
+          <HStack>
+            <InputGroup w="190px" borderColor={colors.grey_dark}>
+              <InputLeftElement pointerEvents="none" h={8}>
+                <SearchIcon color={colors.grey_dark} boxSize={4} />
+              </InputLeftElement>
+              <Input
+                w={40}
+                h={8}
+                onChange={({ target: { value } }) => setSearchFilter(value)}
+              />
+            </InputGroup>
+            <Button
+              color={colors.grey_dark}
+              bg={colors.white}
+              outlineColor={colors.grey_dark}
               h={8}
-              onChange={({ target: { value } }) => setSearchFilter(value)}
-            />
-          </InputGroup>
-          <Button
-            color={colors.grey_dark}
-            bg={colors.white}
-            outlineColor={colors.grey_dark}
-            h={8}
-            onClick={() => {
-              onModalOpen();
-            }}
-          >
-            <IoFunnelOutline pointerEvents={"none"} />
-            &nbsp; Filter
-          </Button>
-        </HStack>
+              onClick={() => {
+                onModalOpen();
+              }}
+            >
+              <IoFunnelOutline pointerEvents={"none"} />
+              &nbsp; Filter
+            </Button>
+          </HStack>
+        )}
       </HStack>
       {isSuccess && (
         <DataTable
-          columns={columns}
+          columns={pendingColumns(navigate)}
           data={data?.results ?? []}
           filter={{ globalFilter: searchFilter }}
           pagination={{
