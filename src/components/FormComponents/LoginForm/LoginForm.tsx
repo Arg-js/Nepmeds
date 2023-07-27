3c336ddfee3588ba134e8416750a90dc1e067f3a
invictious <developer.invivtious@gmail.com>
2023-07-27 18:10:05 +0545

    fix(merge): merge from master

--- src/components/FormComponents/LoginForm/LoginForm.tsx
+++ src/components/FormComponents/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, HStack, Icon, Text, VStack } from "@chakra-ui/react";
+import { Button, HStack, Icon, Text, VStack } from "@chakra-ui/react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import Input from "@nepMeds/components/Form/Input";
 import { useLoginMutation } from "@nepMeds/service/nepmeds-auth";
@@ -46,81 +46,79 @@ const LoginForm = () => {
   };
 
   return (
-    <Box overflow={"hidden"} h={"100%"}>
-      <form style={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
-        <VStack gap={7} mb={3}>
-          <Input
-            name="email"
-            register={register}
-            startIcon={
-              <Icon as={Message} fontSize={20} color={colors.black_40} />
-            }
-            border="none"
-            backgroundColor={colors.forminput}
-            placeholder="Email Address/ Mobile No."
-            _placeholder={{ color: colors.light_gray }}
-            error={errors.email?.message}
-          />
-          <Input
-            name="password"
-            register={register}
-            type={showPassword ? "text" : "password"}
-            startIcon={<Icon as={Lock} fontSize={20} color={colors.black_40} />}
-            endIcons={
-              <Icon
-                as={showPassword ? Show : Hide}
-                fontSize={20}
-                onClick={togglepasswordView}
-                cursor="pointer"
-              />
-            }
-            backgroundColor={colors.forminput}
-            border="none"
-            placeholder="Password"
-            _placeholder={{ color: colors.light_gray }}
-            error={errors.password?.message}
-          />
-        </VStack>
+    <form style={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
+      <VStack gap={7} mb={3}>
+        <Input
+          name="email"
+          register={register}
+          startIcon={
+            <Icon as={Message} fontSize={20} color={colors.black_40} />
+          }
+          border="none"
+          backgroundColor={colors.forminput}
+          placeholder="Email Address/ Mobile No."
+          _placeholder={{ color: colors.light_gray }}
+          error={errors.email?.message}
+        />
+        <Input
+          name="password"
+          register={register}
+          type={showPassword ? "text" : "password"}
+          startIcon={<Icon as={Lock} fontSize={20} color={colors.black_40} />}
+          endIcons={
+            <Icon
+              as={showPassword ? Show : Hide}
+              fontSize={20}
+              onClick={togglepasswordView}
+              cursor="pointer"
+            />
+          }
+          backgroundColor={colors.forminput}
+          border="none"
+          placeholder="Password"
+          _placeholder={{ color: colors.light_gray }}
+          error={errors.password?.message}
+        />
+      </VStack>
 
+      <Link
+        to="/forgot-password"
+        style={{
+          color: colors.blue_100,
+          display: "block",
+          textAlign: "right",
+          marginBottom: "48px",
+          fontSize: "14px",
+        }}
+      >
+        Forgot Password
+      </Link>
+
+      <Text textAlign="center" fontSize={14} color={colors.black_30}>
+        Don’t have an account?
         <Link
-          to="/forgot-password"
+          to="/signup"
           style={{
             color: colors.blue_100,
-            display: "block",
-            textAlign: "right",
-            marginBottom: "48px",
-            fontSize: "14px",
+            marginLeft: "5px",
           }}
         >
-          Forgot Password
+          Sign Up
         </Link>
+      </Text>
 
-        <Text textAlign="center" fontSize={14} color={colors.black_30}>
-          Don’t have an account?
-          <Link
-            to="/signup"
-            style={{
-              color: colors.blue_100,
-              marginLeft: "5px",
-            }}
-          >
-            Sign Up
-          </Link>
-        </Text>
-
-        <HStack mt={10} justifyContent="center">
-          <Button
-            backgroundColor={colors.primary}
-            textColor={colors.white}
-            type="submit"
-            isLoading={loginAction.isLoading}
-            variant="register"
-          >
-            Login
-          </Button>
-        </HStack>
-      </form>
-    </Box>
+      <HStack mt={10} justifyContent="center">
+        <Button
+          backgroundColor={colors.primary}
+          textColor={colors.white}
+          type="submit"
+          isLoading={loginAction.isLoading}
+          variant="register"
+        >
+          Login
+        </Button>
+      </HStack>
+    </form>
   );
 };
 
