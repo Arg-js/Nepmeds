c24dbe11dfa2de106b3ce6d2bb9ead843a946317
jenishakhaigulicf <jenisha.khaiguli@cloudfactory.com>
2023-08-13 11:29:42 +0545

    Update WrapperBox and Header section

--- src/components/Patient/DoctorConsultation/WrapperBox/index.tsx
+++ src/components/Patient/DoctorConsultation/WrapperBox/index.tsx
@@ -11,16 +11,22 @@ interface IWrapperBox {
   width?: string;
   borderColor?: string;
   border?: string;
+  borderRadius?: string;
+  paddingTop?: string;
+  boxShadow?: string;
 }
 const WrapperBox: React.FC<IWrapperBox> = ({
   children,
   backgroundColor = "none",
   backgroundImage = "none",
   height = "auto",
+  paddingTop = "none",
   padding = "10",
   paddingX = "30",
   width = "auto",
   border = "none",
+  borderRadius = "none",
+  boxShadow = "none",
 }) => {
   return (
     <Box
@@ -29,9 +35,12 @@ const WrapperBox: React.FC<IWrapperBox> = ({
       fontFamily={"Quicksand"}
       backgroundImage={backgroundImage}
       p={padding}
+      pt={paddingTop}
       px={paddingX}
       width={width}
       border={border}
+      borderRadius={borderRadius}
+      boxShadow={boxShadow}
     >
       {children}
     </Box>
