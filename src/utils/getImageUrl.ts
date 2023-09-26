d4fbe912ccca180075155da6eb1ac45b5f4e9e2f
Samishan11 <samishanthapa0@gmail.com>
2023-09-26 14:57:27 +0545

    fix: register bug fix in academic form

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,7 +1,7 @@
 import { normalURL } from "@nepMeds/service/service-axios";
 
 export const getImageUrl = (filePath: string) =>
-  `${normalURL}/media/${filePath}`;
+  `${normalURL.replace("/backend", "")}/media/${filePath}`;
 
 // Accepts with '/' in the beginning e.g. '/media/doctor_profile/doctor_profile_1.jpg'
 export const appendServerUrl = (filePath: string) => `${normalURL}${filePath}`;
