a71853ccc1ca8197f8e557790f5be8af65e44ec7
jenishakhaigulicf <jenisha.khaiguli@cloudfactory.com>
2023-09-27 16:53:12 +0545

    refactor: resolve PR comments

--- src/components/Patient/DoctorConsultation/WrapperBox/index.tsx
+++ src/components/Patient/DoctorConsultation/WrapperBox/index.tsx
@@ -5,42 +5,23 @@ import { colors } from "@nepMeds/theme/colors";
 
 interface IWrapperBox extends BoxProps {
   children?: ReactJSXElement;
-  backgroundColor?: string;
-  backgroundImage?: string;
-  height?: string;
-  padding?: string;
-  width?: string;
-  borderColor?: string;
-  border?: string;
-  borderRadius?: string;
-  boxShadow?: string;
-  px?: string;
   style?: Record<string, any>;
 }
-const WrapperBox: React.FC<IWrapperBox> = ({
-  children,
-  backgroundColor = colors.white,
-  backgroundImage = "none",
-  height = "auto",
-  padding = "4",
-  width = "auto",
-  border = "none",
-  boxShadow = "none",
-  style,
-  ...rest
-}) => {
+const WrapperBox: React.FC<IWrapperBox> = ({ children, style, ...rest }) => {
+  const sx = {
+    backgroundColor: colors.white,
+    backgroundImage: "none",
+    height: "auto",
+    padding: "4",
+    width: "auto",
+    border: "none",
+    boxShadow: "none",
+    fontFamily: "Quicksand",
+  };
   return (
     <Box
-      height={height}
-      bg={backgroundColor}
-      fontFamily={"Quicksand"}
-      backgroundImage={backgroundImage}
-      p={padding}
       px={{ base: "10px", md: "15", xl: "15", "2xl": "20" }}
-      width={width}
-      border={border}
-      boxShadow={boxShadow}
-      sx={style}
+      sx={{ ...sx, ...style }}
       {...rest}
     >
       {children}
