609ba46cd44d4de9cb12c80b470d660bcd168bde
prabesh <prabesh.pathak@codehimalaya.net>
2023-12-11 17:09:54 +0545

    fix: hotfix calendar design

--- src/pages/Admin/Appointments/index.tsx
+++ src/pages/Admin/Appointments/index.tsx
@@ -13,17 +13,11 @@ const AdminAppointment = () => {
         <TabList border={"none"}>
           <Tab>Appointment</Tab>
           <Tab>Instant Consultant</Tab>
-          <Tab>Follow Up</Tab>
-          <Tab>Instant Consultant Request</Tab>
         </TabList>
 
         <TabPanels>
           <TabPanel>{activeTab === 0 && <Appointment />}</TabPanel>
           <TabPanel>{activeTab === 1 && <InstantConsultant />}</TabPanel>
-          <TabPanel>{activeTab === 2 && <p>Consultant</p>}</TabPanel>
-          <TabPanel>
-            {activeTab === 3 && <p>Instant Consultant Request</p>}
-          </TabPanel>
         </TabPanels>
       </Tabs>
     </BoxWrapper>
